f29cd2e10daea6c95bffaf9c6f18c35f6484c772
Przemyslaw Karczmarczyk <przemyslaw.karczmarczyk@cern.ch>
2020-08-27 17:09:22 +0200

    Added first version of yield calculation, no event counter in EventPool

--- EventPool.py
+++ EventPool.py
@@ -1,29 +1,25 @@
 class EventPool():
     # EventPool is a class for keeping a list of tracks
     # for event mixing procedure for correlation analysis
-    def __init__(self, targeteventsize=0, targettracksize=0):
+    def __init__(self, targettracksize=0):
         # Event pool has:
         # - a size that must be reached before event mixing can start
-        self.targeteventsize = targeteventsize
         self.targettracksize = targettracksize
         # - a pool of previous events
         self.eventpool = []
     
     # Method for checking if event pool is ready
     def is_ready(self):
-        if len(self.eventpool) >= self.targeteventsize:
-            print("Event pool - target event size reached: " + str(len(self.eventpool)))
-            if sum([len(event) for event in self.eventpool]) >= self.targettracksize:
-                print("Event pool - target track size reached: " + str(sum([len(event) for event in self.eventpool])))
-                return True
+        if sum([len(event) for event in self.eventpool]) >= self.targettracksize:
+            print("Event pool - target track size reached: " + str(sum([len(event) for event in self.eventpool])))
+            return True
         return False
     
     # Method for adding a new event to the pool
     def add(self, newevent):
         self.eventpool.append(newevent)
-        if len(self.eventpool) > self.targeteventsize:
-            if sum([len(event) for event in self.eventpool]) > self.targettracksize:
-                self.eventpool.pop()
+        if sum([len(event) for event in self.eventpool]) - len(self.eventpool[0]) > self.targettracksize:
+            self.eventpool.pop(0)
 
     # Method for getting a list of all tracks in event pool
     def trackpool(self):
--- yield-generator.py
+++ yield-generator.py
@@ -1,21 +1,22 @@
 import math
 import random
-from ROOT import gROOT, gRandom, gSystem, TCanvas, TFile, TH1F, TH2F
+from ROOT import gROOT, gRandom, gSystem, TCanvas, TFile, TH1F, TH2F, TLatex, TString
 from Particle import Particle
 from ParticlePair import ParticlePair
 from EventPool import EventPool
 
 # Generator settings
-nev = 50
+nev = 1000
 npart = 100
 # Generator settings for event mixing
-targeteventsize = 5
-targettracksize = 100
+targettracksize = 1000
 
 # Generate dataset
 dataset = []
 for iev in range(0,nev):
     event = []
+    npart = random.randint(80, 120)
+    print(str(npart))
     for ii in range(0,npart):
         pt = random.uniform(0.5, 1.0)
         eta = random.uniform(-0.8, 0.8)
@@ -25,11 +26,14 @@ for iev in range(0,nev):
 
 # Prepare variables
 ievent = 0
+ntrigger = 0
 pi = math.pi
-ep = EventPool(targeteventsize, targettracksize)
-hsame = TH2F('hsame', 'Same events histogram', 32, -1.6, 1.6, 32, -0.5*math.pi, 1.5*math.pi)
-hmixed = TH2F('hmixed', 'Mixed events histogram', 32, -1.6, 1.6, 32, -0.5*math.pi, 1.5*math.pi)
-hyield = TH2F('hyield', 'Associated yield per trigger particle', 32, -1.6, 1.6, 32, -0.5*math.pi, 1.5*math.pi)
+ep = EventPool(targettracksize)
+wdphi = pi / 16
+wdeta = 0.1
+hsame = TH2F('hsame', 'Same events histogram', 32, -1.6, 1.6, 32, -0.5*pi, 1.5*pi)
+hmixed = TH2F('hmixed', 'Mixed events histogram', 32, -1.6, 1.6, 32, -0.5*pi, 1.5*pi)
+hyield = TH2F('hyield', 'Associated yield per trigger particle', 32, -1.6, 1.6, 32, -0.5*pi, 1.5*pi)
 csame = TCanvas('csame', 'Same events canvas', 200, 10, 700, 500)
 cmixed = TCanvas('cmixed', 'Mixed events canvas', 200, 10, 700, 500)
 cyield = TCanvas('cyield', 'Yield canvas', 200, 10, 700, 500)
@@ -38,6 +42,7 @@ cyield = TCanvas('cyield', 'Yield canvas', 200, 10, 700, 500)
 for event in dataset:
     print(ievent)
     for trig in event:
+        ntrigger += 1
         for assoc in event:
             if trig == assoc:
                 continue
@@ -66,6 +71,28 @@ for event in dataset:
     ep.add(event)
     ievent += 1
 
+# Calculate yield factors
+print("ntrigger: " + str(ntrigger))
+alpha = 1. / (wdphi * ntrigger)
+print("alpha: " + str(alpha))
+f = 1. - 1. / (2. * 0.8) * wdeta / 2.
+print("f: " + str(f))
+xmin = hmixed.GetXaxis().FindBin(-wdeta+0.001)
+xmax = hmixed.GetXaxis().FindBin(wdeta-0.001)
+ymin = hmixed.GetYaxis().FindBin(-wdphi+0.001)
+ymax = hmixed.GetYaxis().FindBin(wdphi-0.001)
+mixedFactor = hmixed.Integral(xmin, xmax, ymin, ymax)
+print("mixedFactor: " + str(mixedFactor))
+gamma = 4. * f / mixedFactor
+print("gamma: " + str(gamma))
+
+# Calculate final yield
+#hyield = hsame.Clone()
+hyield.Add(hsame)
+hyield.Divide(hmixed)
+print("alpha / gamma: " + str(alpha / gamma))
+hyield.Scale(alpha / gamma)
+
 # Plotting and saving
 csame.cd()
 hsame.Draw("colz")
@@ -78,3 +105,13 @@ hmixed.Draw("colz")
 cmixed.Modified()
 cmixed.Update()
 cmixed.Print()
+
+cyield.cd()
+hyield.Draw("colz")
+integral = hyield.Integral() / (32.*32.)
+print(str(integral))
+panelIntegral = TLatex(-0.5, 1.5, str(integral));
+panelIntegral.Draw()
+cyield.Modified()
+cyield.Update()
+cyield.Print()
